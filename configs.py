1e10ee87e44b34927730acc555f4b38062ef8b2c
SakuraSa <sakura_kyon@hotmail.com>
2014-11-05 21:41:13 +0800

    add user config file

--- configs.py
+++ configs.py
@@ -3,6 +3,7 @@
 
 __author__ = 'Rnd495'
 
+import os
 import hashlib
 import tornado.options
 
@@ -29,3 +30,24 @@ with open("markdown-help.md", "r") as file_handler:
 
 #nav tabs
 blog_nav_tabs = [(u'聊天室', '/'), (u'文章', '/post/list'), (u'图片', '/image/list')]
+
+#load from user file
+setting_file_path = "setting.conf"
+file_config = {}
+if os.path.exists(setting_file_path):
+    with open(setting_file_path) as file_handle:
+        for line in file_handle:
+            line = line.strip()
+            if (not line) or line.startswith("#"):
+                continue
+            parts = line.split("=")
+            if len(parts) < 1:
+                print "Config Error: config syntax error"
+                print "              %s" % line
+                continue
+            name = parts[0]
+            value = line[line.index("=") + 1:].strip()
+            cmd = "%s = %s" % (name, value)
+            exec cmd
+            print cmd
+
